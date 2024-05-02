c7249bf3f3ce52ba5110949bd1ded14be03f1adc
chenwehle <4b0g0017@stust.edu.tw>
2024-05-03 00:01:05 +0800

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import MyRGBPanel from './component/MyRGBPanel'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>4b0g0017陳俊霖:Hello React!</h1>
+        <h2>React第二個元件</h2>
+        <MyRGBPanel />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/MySlider.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+function MySlider({label, onChange}){
+    const [value, setValue] = useState(128);
+    const handleChange = (e) =>{
+        setValue(e.target.value);
+
+        if(onChange){
+            onChange(e.target.value);
+        }
+    };
+    return(
+        <div>
+            <span>{label}</span>
+            <input type ="range" width="300" min="0" max="255" value={value} onChange={handleChange}/>
+            <span>{value}</span>
+        </div>
+    );
+}
+export default MySlider;
